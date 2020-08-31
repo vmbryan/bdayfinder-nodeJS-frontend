ea6bfbd2ad6911f9d17e09a8a348360bd88f2df8
vmbryan <55796603+vmbryan@users.noreply.github.com>
2020-09-01 01:23:35 +0200

    Fixed redirect after signup

--- js/signup.js
+++ js/signup.js
@@ -28,7 +28,7 @@ const btnSignup = document.querySelector(".signup__btn").addEventListener("click
 
             let token = json.data.token
             localStorage.setItem("token", token)
-            window.location.href = "app.html"
+            window.location.href = "index.html"
         }
     })
 })
